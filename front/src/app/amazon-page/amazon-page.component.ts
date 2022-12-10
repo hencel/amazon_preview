0730bce8fd48efdce616febd64711054ddcbac08
Zbigniew Piernik <zbigniewpiernik@gmail.com>
2022-12-10 20:45:55 +0100

    001 Structure of the front project. Added components and routing

---
+++ front/src/app/amazon-page/amazon-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-amazon-page',
+  templateUrl: './amazon-page.component.html',
+  styleUrls: ['./amazon-page.component.scss']
+})
+export class AmazonPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoggingComponent } from './logging/logging.component';
+import { MainFormComponent } from './main-form/main-form.component';
+import { ReadyPageComponent } from './ready-page/ready-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'logging', component: LoggingComponent },
+  { path: 'main-form', component: MainFormComponent},
+  { path: '', component: LoggingComponent},
+  { path: '**', component: ReadyPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
