c63db3aa6586d893b17e15b464adadd3986368c2
Zbigniew Piernik <zbigniewpiernik@gmail.com>
2022-12-10 21:02:54 +0100

    001 Added guide post component and links page component

---
+++ front/src/app/guide-post/guide-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-guide-post',
+  templateUrl: './guide-post.component.html',
+  styleUrls: ['./guide-post.component.scss']
+})
+export class GuidePostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ front/src/app/links-catalog/links-catalog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-links-catalog',
+  templateUrl: './links-catalog.component.html',
+  styleUrls: ['./links-catalog.component.scss']
+})
+export class LinksCatalogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
