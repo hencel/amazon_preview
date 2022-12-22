503b0b3e06f1fafa63697120ee2ab9f949a9574a
Zbigniew Piernik <zbigniewpiernik@gmail.com>
2022-12-22 22:47:53 +0100

    002 Added input for upload main image files

--- front/src/app/main-form/main-form.component.ts
+++ front/src/app/main-form/main-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, NgModule } from '@angular/core';
-import { faArrowRight, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight, faPlus, faMinus, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { SpecItem, BulletPoint } from '../model/interfaces';
 // import { NgForm } from '@angular/forms';
 import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
@@ -15,8 +15,9 @@ export class MainFormComponent implements OnInit {
   arrowRightIcon = faArrowRight;
   plusIcon = faPlus;
   minusIcon = faMinus;
+  checkIcon = faCheckCircle;
 
-  fileMain: string = '';
+  mainFileText = 'Wgraj plik (970px szerokości) <img src="assets/images/icon-image.svg" />';
 
   filesSecondary: BulletPoint[] = [{
     text: ''
@@ -33,7 +34,8 @@ export class MainFormComponent implements OnInit {
       ]),
       bulletPointsArray: this.builder.array([
         this.initBullet(),
-      ])
+      ]),
+      fileMain: [null]
     });
   }
 
@@ -55,7 +57,7 @@ export class MainFormComponent implements OnInit {
     return (this.myForm.get(target) as FormArray).controls;
   }
  
-  addDataRow(target: string) {
+  addDataRow(target: string): void {
     const control = <FormArray>this.myForm.controls[target];
     switch (target) {
       case 'specDataArray':
@@ -69,11 +71,18 @@ export class MainFormComponent implements OnInit {
     }
   }
 
-  removeDataRow(target: string, i: number) {
+  removeDataRow(target: string, i: number): void {
     const control = <FormArray>this.myForm.controls[target];
     control.removeAt(i);
   }
 
+  uploadFile(event: Event): void {
+    const file = (event.target as HTMLInputElement)?.files?.[0];
+    this.myForm.patchValue({
+      fileMain: file
+    });
+    this.myForm.get('fileMain')?.setValue(file); //updateValueAndValidity()
+  }
 
   // uploadFile(event: Event) {
   //   const element = event.currentTarget as HTMLInputElement;
@@ -91,26 +100,27 @@ export class MainFormComponent implements OnInit {
   //     this.customFile = target.files[0].name;
   //   }
   // }
-  onFileSelected(event: Event) {
-    const file = (event.target as HTMLInputElement)?.files?.[0];
+//   onFileSelected(event: Event) {
+//     const file = (event.target as HTMLInputElement)?.files?.[0];
 
-    if (file) {
-      console.log((event.target as Element).id);
-      this.fileMain = file.name;
-      // const formData = new FormData();
-      // formData.append("thumbnail", file);
-      // const upload$ = this.http.post("/api/thumbnail-upload", formData);
-      // upload$.subscribe();
-    }
+//     if (file) {
+//       console.log((event.target as Element).id);
+//       this.fileMain = file.name;
+//       // const formData = new FormData();
+//       // formData.append("thumbnail", file);
+//       // const upload$ = this.http.post("/api/thumbnail-upload", formData);
+//       // upload$.subscribe();
+//     }
 
     
-}
+// }
 
   submitForm() {
     let formData: any = new FormData();
     formData.append('mainFormName', this.myForm.get('mainFormName')?.value);
     formData.append('mainFormTitle', this.myForm.get('mainFormTitle')?.value);
     // formData.append('avatar', this.form.get('avatar').value);
+
     console.log(this.myForm);
   }
 }
