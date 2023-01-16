7c0ea8b610328a588fcd7df6f63b062fa014783b
Zbigniew Piernik <zbigniewpiernik@gmail.com>
2023-01-16 23:12:15 +0100

    002 Prepared post function to login auth and services to set, get localstorage keys

--- front/src/app/service/project.service.ts
+++ front/src/app/service/project.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoginToken, LoginObject } from '../model/interfaces';
 
 @Injectable({
   providedIn: 'any',
 })
 export class ProjectService {
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   data: Object = {};
 
-  passReadyData(res: Object) {
+  public passReadyData(res: Object) {
     this.data = res;
   }
 
+  public postAuth(url: string, data: LoginObject) {
+    return this.http.post<LoginToken>(url, data);
+  }
+
+  public saveLocalStorage(key: string, value: string) {
+    localStorage.setItem(key, value);
+  }
+
+  public getLocalStorage(key: string) {
+    return localStorage.getItem(key)
+  }
 }
