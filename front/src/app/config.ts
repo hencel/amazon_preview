2eff189bdcbc1796c50fd65c3ccfe326b540e99d
Zbigniew Piernik <zbigniewpiernik@gmail.com>
2023-01-16 21:28:50 +0100

    002 Added config address to login. Added http post to make login

--- front/src/app/config.ts
+++ front/src/app/config.ts
@@ -1,4 +1,6 @@
 export const mainAddress = 'https://amazon.rmadev.pl/api';
+//Post login
+export const login = '/login_check';
 //Post and get products from db
 export const products = '/products';
 //Post and get medias from db
